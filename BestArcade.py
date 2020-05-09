367e72025ef9bb4b5bb64a06f8130adefba219f2
Voljega <voljega@gmail.com>
2020-05-09 19:39:38 +0200

    better icon

--- BestArcade.py
+++ BestArcade.py
@@ -16,10 +16,13 @@ if __name__ == "__main__":
 # TODO
 1.4 :
 -------
-# retest ALL after build
+# retest ALL after build -> test new build with
 
 1.5:
 ----
+# add vertical games tab
+# strange bug where .ini files got uppercase for naomi and atomiswave, maybe on intellij side, might be a problem on linux
 # solve compilation questions and try on an ubuntu VM
 # add info to generate dat from mame exe (see my handheld thread on reddit) -> hhhhhhm si if regular xml from mame site work
-# allow basicsorter generation without dat / prevents generation without dat ? 
+# allow basicsorter generation without dat / prevents generation without dat ?
+
